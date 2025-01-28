02163b47852a19df009bf3a4660b6081be7ca5fa
Moorthy <moorthy.chandiran21@gmail.com>
2025-01-28 17:25:23 +0530

    change in date

--- src/base/BaseUrl.jsx
+++ src/base/BaseUrl.jsx
@@ -1,5 +1,4 @@
 const BASE_URL = "https://agsl.online/public";
-// const BASE_URL = "https://test.dfclogistics.online/public";
 
 export const TEAM_URL = "https://dfclogistics.online/storage/app/public";
 
