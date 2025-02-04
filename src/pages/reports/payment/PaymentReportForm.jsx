15c1d40eb8d6dba3ff621e36918bf9826a377348
Moorthy <moorthy.chandiran21@gmail.com>
2025-02-04 16:01:31 +0530

    chnages in the payment report page

--- src/pages/reports/payment/PaymentReportForm.jsx
+++ src/pages/reports/payment/PaymentReportForm.jsx
@@ -7,7 +7,10 @@ import SelectInput from "../../../components/common/SelectField";
 import { useNavigate } from "react-router-dom";
 import { IconInfoCircle } from "@tabler/icons-react";
 import moment from "moment";
-import { ReportPaymentDownload, ReportPaymentView } from "../../../components/buttonIndex/ButtonComponents";
+import {
+  ReportPaymentDownload,
+  ReportPaymentView,
+} from "../../../components/buttonIndex/ButtonComponents";
 
 function PaymentReportForm() {
   const navigate = useNavigate();
@@ -20,14 +23,29 @@ function PaymentReportForm() {
     payment_details_credit: " ",
   });
 
-  const onInputChange = (selectedOption, action) => {
-    console.log("Selected Option:", selectedOption);
-    console.log("Action:", action);
+  // const onInputChange = (selectedOption, action) => {
+  //   console.log("Selected Option:", selectedOption);
+  //   console.log("Action:", action);
 
-    setPaymentDetailsDownload((prevState) => ({
-      ...prevState,
-      [action.name]: selectedOption ? selectedOption.value : "",
-    }));
+  //   setPaymentDetailsDownload((prevState) => ({
+  //     ...prevState,
+  //     [action.name]: selectedOption ? selectedOption.value : "",
+  //   }));
+  // };
+
+  const onInputChange = (eventOrOption, action) => {
+    if (action) {
+      setPaymentDetailsDownload((prevState) => ({
+        ...prevState,
+        [action.name]: eventOrOption ? eventOrOption.value : "",
+      }));
+    } else {
+      const { name, value } = eventOrOption.target;
+      setPaymentDetailsDownload((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
   };
 
   const fetchBranches = async () => {
@@ -203,14 +221,11 @@ function PaymentReportForm() {
                 onClick={onSubmit}
               >
                 {" "}
-           
               </ReportPaymentDownload>
               <ReportPaymentView
                 className=" text-center text-sm font-[400 ] cursor-pointer hover:animate-pulse w-36 text-white bg-blue-600 hover:bg-green-700 p-2 rounded-lg shadow-md ml-4"
                 onClick={handleview}
-              >
-        
-              </ReportPaymentView>
+              ></ReportPaymentView>
             </div>
           </form>
         </div>
