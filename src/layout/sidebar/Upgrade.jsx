2675eea26e2616db38dd8d5266ec80e109b5b486
Moorthy <moorthy.chandiran21@gmail.com>
2025-04-11 18:26:14 +0530

    chnage in the baseurl

--- src/layout/sidebar/Upgrade.jsx
+++ src/layout/sidebar/Upgrade.jsx
@@ -1,27 +1,7 @@
-// import { Box } from "@mui/material";
-
-// export const Upgrade = ({ isCollapsed }) => {
-//   return (
-//     <>
-//       {!isCollapsed ? (
-//         <Box display={"flex"} alignItems="center" gap={2} sx={{ m: 3  }}>
-//           <>
-//             <h2 className="text-xs  text-red-600/80 border-b-2 border-dashed border-blue-600/80">
-//               Updated On: 11-03-2025
-//             </h2>
-
-//           </>
-//         </Box>
-//       ) : (
-//         ""
-//       )}
-//     </>
-//   );
-// };
 import { Box } from "@mui/material";
 
 export const Upgrade = ({ isCollapsed }) => {
-  const text = "Updated On: 11-03-2025";
+  const text = "Updated On: 11-04-2025";
 
   return (
     <>
@@ -33,7 +13,7 @@ export const Upgrade = ({ isCollapsed }) => {
                 key={index}
                 className="inline-block animate-waving"
                 style={{
-                  animationDelay: `${index * 100}ms`, 
+                  animationDelay: `${index * 100}ms`,
                 }}
               >
                 {char}
